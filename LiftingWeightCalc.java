ace1d6de47886b6a3051275fa83b5eccae265fbd
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-10-12 17:11:05 +0300

    Add files via upload

---
+++ LiftingWeightCalc.java
@@ -0,0 +1,11 @@
+package LiftingWeightCalc;
+
+public class LiftingWeightCalc {
+static int bw = 80;
+static int multiply= 2;
+static int lw = bw*multiply;
+
+public static void main(String[] args) {
+	System.out.println("Your Lifting weight is:"+ lw);
+}
+}
